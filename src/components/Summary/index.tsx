bc061ebf53e322912530cbd0184b271359f40d88
Gabriel <gabrieldiasbnu@hotmail.com>
2021-05-02 21:19:17 +0200

    feat: set summary data coming from hook useTransactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,33 @@
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container } from './styles';
 
 export function Summary() {
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      switch (transaction.transactionType) {
+        case 'deposit':
+          acc.deposits += transaction.amount;
+          acc.total += transaction.amount;
+          break;
+        case 'withdraw':
+          acc.withdraws += transaction.amount;
+          acc.total -= transaction.amount;
+          break;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -11,21 +35,37 @@ export function Summary() {
           <p>Incomes</p>
           <img src={incomeImg} alt='Incomes' />
         </header>
-        <strong>€1500</strong>
+        <strong>
+          {new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR',
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Outcomes</p>
           <img src={outcomeImg} alt='Outcomes' />
         </header>
-        <strong>- €500</strong>
+        <strong>
+          -{' '}
+          {new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR',
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className='highlight-background'>
         <header>
           <p>Total</p>
           <img src={totalImg} alt='Total' />
         </header>
-        <strong>- €500</strong>
+        <strong>
+          {new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR',
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
