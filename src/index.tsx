b62d844482b4ff3180d4df414f1efd6eca905286
Gabriel <gabrieldiasbnu@hotmail.com>
2021-05-02 21:18:20 +0200

    test: add initial items to array for testing purposes

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance Website',
+          transactionType: 'deposit',
+          category: 'Software Development',
+          amount: 5000,
+          createdAt: new Date('2021-05-12 09:50:00'),
+        },
+        {
+          id: 2,
+          title: 'Pizza',
+          transactionType: 'withdraw',
+          category: 'Spends',
+          amount: 30,
+          createdAt: new Date('2021-10-10 19:00:00'),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          value: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all('transaction');
+    });
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     });
   },
 });
